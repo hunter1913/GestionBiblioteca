a6e4740cc456885c38e44c4756e3b837dddde1bb
Andretty <felipe.andrey04@gmail.com>
2024-11-02 00:46:11 -0500

    Formulario devoluciones backend

---
+++ src/main/java/com/devpipe/GestionBiblioteca/gui/DevolucionesForma.java
@@ -0,0 +1,12 @@
+package com.devpipe.GestionBiblioteca.gui;
+
+import javax.swing.*;
+
+public class DevolucionesForma {
+    private JTextField idPrestamoText;
+    private JTable prestamosDevolucionesTabla;
+
+    private void createUIComponents() {
+
+    }
+}
