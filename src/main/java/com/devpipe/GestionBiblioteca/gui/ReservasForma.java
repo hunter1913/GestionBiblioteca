367ecdf6792c9280879e9566ac43bd5325578378
Andretty <felipe.andrey04@gmail.com>
2024-11-08 02:39:10 -0500

    Reservas Backend

--- src/main/java/com/devpipe/GestionBiblioteca/gui/ReservasForma.java
+++ src/main/java/com/devpipe/GestionBiblioteca/gui/ReservasForma.java
@@ -1,5 +1,6 @@
 package com.devpipe.GestionBiblioteca.gui;
 
+import com.devpipe.GestionBiblioteca.modelo.Reserva;
 import com.devpipe.GestionBiblioteca.servicio.IReservaServicio;
 import com.devpipe.GestionBiblioteca.servicio.ReservaServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,13 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 public class ReservasForma extends JFrame{
@@ -14,15 +22,35 @@ public class ReservasForma extends JFrame{
     private JButton menuPrincipalButton;
     private JTable reservasTabla;
     private JButton hacerPrestamoButton;
+    private JTextField fechaReservaTexto;
+    private JTextField libroTexto;
+    private JTextField socioTexto;
+    private JButton registrarReservaButton;
     IReservaServicio reservaServicio;
     private DefaultTableModel tablaModeloReservas;
+    private PrestamosForma prestamosForma;
+    private Integer idReserva;
+    private String estadoReserva;
+    private Integer idLibro;
+    private Integer idSocio;
+
 
     @Autowired
-    public ReservasForma(ReservaServicio reservaServicio) {
+    public ReservasForma(ReservaServicio reservaServicio, PrestamosForma prestamosForma) {
         this.reservaServicio = reservaServicio;
+        this.prestamosForma = prestamosForma;
         iniciarForma();
 
         menuPrincipalButton.addActionListener(e -> menuPrincipal());
+        fechaReservaTexto.setText(prestamosForma.cargarFecha());
+        reservasTabla.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                cargarReservaSeleccionada();
+            }
+        });
+        registrarReservaButton.addActionListener(e -> guardarReserva());
     }
 
     private void iniciarForma(){
@@ -63,9 +91,9 @@ public class ReservasForma extends JFrame{
         reservas.forEach(reserva -> {
                       Object[] renglonReserva = {
                         reserva.getIdReserva(),
-                              reserva.getFechaReserva(),
-                              reserva.getIdLibro(),
-                              reserva.getIdSocio(),
+                             formatoFecha(reserva.getFechaReserva()),
+                              prestamosForma.cambiarIdPorNombreLibro(reserva.getIdLibro()),
+                              prestamosForma.cambiarIdPorNombreSocio(reserva.getIdSocio()),
                               reserva.getEstadoReserva(),
                 };
                 this.tablaModeloReservas.addRow(renglonReserva);
@@ -73,5 +101,55 @@ public class ReservasForma extends JFrame{
         });
     }
 
+    private void cargarReservaSeleccionada() {
+        var renglon = reservasTabla.getSelectedRow();
+        if (renglon != -1) ; // significa que no se selecciono ningun registro
+        var id = reservasTabla.getModel().getValueAt(renglon, 0).toString();
+        this.idReserva = Integer.parseInt(id);
+        var fechaReserva = reservasTabla.getModel().getValueAt(renglon,1).toString();
+        this.fechaReservaTexto.setText(fechaReserva);
+        var reserva = reservaServicio.buscarReservaPorId(this.idReserva);
+        Integer updateCodigoLibro = reserva.getIdLibro();
+        String codigoLibroCadena = String.valueOf(updateCodigoLibro);
+        this.libroTexto.setText(codigoLibroCadena);
+        Integer updateCodigoSocio = reserva.getIdSocio();
+        String codigoSocioCadena = String.valueOf(updateCodigoSocio);
+        this.socioTexto.setText(codigoSocioCadena);
+    }
+
+    private void guardarReserva(){
+        //Recuperamos los valores
+       var fechaReserva = fechaReservaTexto.getText();
+       var idLibro = libroTexto.getText();
+       this.idLibro= Integer.parseInt(idLibro);
+        var idSocio = socioTexto.getText();
+        this.idSocio= Integer.parseInt(idSocio);
+        this.estadoReserva = "Pendiente";
+        Reserva reserva = new Reserva();
+        reserva.setFechaReserva(formatoTipoFecha(fechaReserva));
+        reserva.setIdLibro(this.idLibro);
+        reserva.setIdSocio(this.idSocio);
+        reserva.setEstadoReserva(this.estadoReserva);
+        this.reservaServicio.guardarReserva(reserva);
+        listarReservas();
+
+    }
+
+    private Date formatoTipoFecha(String fecha){
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
+        var fechatexto = fecha;
+        Date fechaFormatiada = null;
+        try {
+            fechaFormatiada = formatoFecha.parse(fechatexto);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return fechaFormatiada;
+    }
+
+    public String formatoFecha(Date fecha){
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
+        return formatoFecha.format(fecha);
+    }
 
 }
